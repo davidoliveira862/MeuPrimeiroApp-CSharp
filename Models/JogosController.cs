103c56b70d527ac6ddb40ddb66dc06f135d60d75
David Oliveira <soaresdavid862@gmail.com>
2026-05-06 21:07:24 -0300

    .Gitignore Criado

--- Models/JogosController.cs
+++ Models/JogosController.cs
@@ -26,22 +26,22 @@ namespace MeuPrimeiroApp.Controllers;
         return CreatedAtAction(nameof(GetTodos), new { id = novoJogo.Id }, novoJogo);
     }
 
-    [HttpDelete("{id}")] // Define que a rota precisa de um ID, ex: api/jogos/1
+    [HttpDelete("{id}")] 
 public ActionResult Delete(int id)
 {
-    // 1. Procura o jogo na lista pelo ID
+    
     var jogo = _jogos.FirstOrDefault(j => j.Id == id);
 
-    // 2. Se não encontrar, retorna um erro 404
+    
     if (jogo == null)
     {
         return NotFound("Jogo não encontrado para deletar.");
     }
 
-    // 3. Se encontrar, remove da lista
+    
     _jogos.Remove(jogo);
 
-    // 4. Retorna 204 (No Content), que é o padrão de sucesso para deleção
+   
     return NoContent();
 }
 }
